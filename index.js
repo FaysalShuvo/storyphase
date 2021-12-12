962bd6a13b178e9f728ba19c40f943cfcf5c186e
faysalshuvo <faysalshuvo.dev@gmail.com>
2021-12-12 22:09:52 +0600

    google auth done

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const { engine } = require("express-handlebars");
+const passport = require("passport");
+const session = require("express-session");
 const connectDB = require("./config/db");
 
 // load config
 dotenv.config({ path: "./config/config.env" });
+
+// passport config
+require("./config/passport")(passport);
+
 connectDB();
 
 const app = express();
@@ -21,10 +27,24 @@ app.engine(".hbs", engine({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", ".hbs");
 app.set("views", "./views");
 
+// sessions
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+// passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 // static
 app.use(express.static(path.join(__dirname, "public")));
 // Routes
 app.use("/", require("./routes/homeRoute"));
+app.use("/auth", require("./routes/auth"));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
---
+++ routes/auth.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const passport = require("passport");
+const router = express.Router();
+
+// auth with google
+// GET /auth/google
+router.get("/google", passport.authenticate("google", { scope: ["profile"] }));
+
+// Google auth callback
+// GET /auth/google/callback
+router.get(
+  "/google/callback",
+  passport.authenticate("google", { failureRedirect: "/" }),
+  (req, res) => {
+    res.redirect("/dashboard");
+  }
+);
+module.exports = router;
