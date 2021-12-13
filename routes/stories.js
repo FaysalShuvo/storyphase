eceaa328160f24d54387d8195df613fd76dd3341
faysalshuvo <faysalshuvo.dev@gmail.com>
2021-12-13 19:19:53 +0600

    shows the stories

--- routes/stories.js
+++ routes/stories.js
@@ -21,4 +21,21 @@ router.post("/", ensureAuth, async (req, res) => {
   }
 });
 
+// show all stories
+router.get("/", ensureAuth, async (req, res) => {
+  try {
+    const stories = await Story.find({ status: "public" })
+      .populate("user")
+      .sort({ createdAt: "desc" })
+      .lean();
+
+    res.render("stories/index", {
+      stories,
+    });
+  } catch (error) {
+    console.error(error);
+    res.render("error/500");
+  }
+});
+
 module.exports = router;
